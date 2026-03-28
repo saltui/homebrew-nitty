b28ba9f5acb29bdc97227404bb3dc044f70191bf
jaden <jaden@jadenui-MacBookPro.local>
2026-03-29 02:07:11 +0900

    update SHA256 for v1.0.0 with app icon

--- Casks/nitty.rb
+++ Casks/nitty.rb
@@ -1,6 +1,6 @@
 cask "nitty" do
   version "1.0.0"
-  sha256 "65027af22affcc987a63cdac8ad81341053372f39f61498701e09b7b4f243e5e"
+  sha256 "0dcd1faf011fd6fa379bdf12385d153c63d25ce743e8f4d0641eda0afcfd194e"
 
   url "https://github.com/saltui/homebrew-nitty/releases/download/v#{version}/Nitty-#{version}.dmg"
   name "Nitty"
