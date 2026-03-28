8d8d6b7b367a4431dcdfc8a4a1a0f7dd034de9bb
jaden <jaden@jadenui-MacBookPro.local>
2026-03-29 00:41:59 +0900

    fix: use homebrew-nitty repo for DMG download (public)

--- Casks/nitty.rb
+++ Casks/nitty.rb
@@ -2,7 +2,7 @@ cask "nitty" do
   version "1.0.0"
   sha256 "65027af22affcc987a63cdac8ad81341053372f39f61498701e09b7b4f243e5e"
 
-  url "https://github.com/saltui/nitty/releases/download/v#{version}/Nitty-#{version}.dmg"
+  url "https://github.com/saltui/homebrew-nitty/releases/download/v#{version}/Nitty-#{version}.dmg"
   name "Nitty"
   desc "XDR brightness boost for Mac — up to 1600 nits"
   homepage "https://github.com/saltui/nitty"
